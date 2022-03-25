ff8c54ece04af061ec21d48dc178a06953ac4acb
Никита Таратин <nikt2001.nt.nt@gmail.com>
2022-03-25 12:55:35 +0300

    Созданы формы.

---
+++ LynaMarketMobile/LunaMarketAdministration/Forms/NewsForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace LunaMarketAdministration.Forms
+{
+    partial class NewsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectImageButton = new System.Windows.Forms.Button();
+            this.TitleTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.editButton = new System.Windows.Forms.Button();
+            this.actionComboBox = new System.Windows.Forms.ComboBox();
+            this.imagePictureBox = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectImageButton
+            // 
+            this.selectImageButton.Location = new System.Drawing.Point(12, 324);
+            this.selectImageButton.Name = "selectImageButton";
+            this.selectImageButton.Size = new System.Drawing.Size(204, 28);
+            this.selectImageButton.TabIndex = 1;
+            this.selectImageButton.Text = "Выбрать изображение";
+            this.selectImageButton.UseVisualStyleBackColor = true;
+            this.selectImageButton.Click += new System.EventHandler(this.SelectImageButtonOnClick);
+            // 
+            // TitleTextBox
+            // 
+            this.TitleTextBox.Location = new System.Drawing.Point(341, 96);
+            this.TitleTextBox.Name = "TitleTextBox";
+            this.TitleTextBox.Size = new System.Drawing.Size(231, 29);
+            this.TitleTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(406, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Заголовок";
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(341, 152);
+            this.descriptionTextBox.Multiline = true;
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(231, 160);
+            this.descriptionTextBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(410, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Описание";
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(452, 324);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(120, 28);
+            this.editButton.TabIndex = 6;
+            this.editButton.Text = "Добавить";
+            this.editButton.UseVisualStyleBackColor = true;
+            // 
+            // actionComboBox
+            // 
+            this.actionComboBox.FormattingEnabled = true;
+            this.actionComboBox.Items.AddRange(new object[] {
+            "Добавление",
+            "Изменение",
+            "Удаление"});
+            this.actionComboBox.Location = new System.Drawing.Point(12, 37);
+            this.actionComboBox.Name = "actionComboBox";
+            this.actionComboBox.Size = new System.Drawing.Size(170, 29);
+            this.actionComboBox.TabIndex = 7;
+            this.actionComboBox.SelectedIndexChanged += new System.EventHandler(this.ActionComboBoxOnSelectedIndexChanged);
+            // 
+            // imagePictureBox
+            // 
+            this.imagePictureBox.Image = global::LunaMarketAdministration.Properties.Resources.no;
+            this.imagePictureBox.Location = new System.Drawing.Point(12, 72);
+            this.imagePictureBox.Name = "imagePictureBox";
+            this.imagePictureBox.Size = new System.Drawing.Size(300, 240);
+            this.imagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imagePictureBox.TabIndex = 0;
+            this.imagePictureBox.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Выберите действие:";
+            // 
+            // NewsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.actionComboBox);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TitleTextBox);
+            this.Controls.Add(this.selectImageButton);
+            this.Controls.Add(this.imagePictureBox);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "NewsForm";
+            this.Text = "Новости";
+            this.Load += new System.EventHandler(this.NewsFormOnLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imagePictureBox;
+        private System.Windows.Forms.Button selectImageButton;
+        private System.Windows.Forms.TextBox TitleTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.ComboBox actionComboBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
